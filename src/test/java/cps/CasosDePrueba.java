946ab2ecc56a46c17301920ef7306f180beab0e4
cmoraga09 <cmoraga.ochoa@gmail.com>
2023-12-03 00:10:12 -0300

    Actualizacion caso de prueba 5

--- src/test/java/cps/CasosDePrueba.java
+++ src/test/java/cps/CasosDePrueba.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
@@ -44,7 +45,7 @@ public class CasosDePrueba {
     @Disabled
     @Test
     @Order(1)
-    public void CP001_validar_creacion_cuenta_nueva() throws InterruptedException {
+    public void CP001_crear_cuenta_nueva() throws InterruptedException {
 
         By localizadorMiCuenta = By.xpath(" //span[@class='caret']");
         Thread.sleep(2000);
@@ -87,7 +88,7 @@ public class CasosDePrueba {
     @Disabled
     @Test
     @Order(2)
-    public void CP002_validar_creacion_cuenta_antigua() throws InterruptedException {
+    public void CP002_validar_mensaje_creacion_cuenta_existente() throws InterruptedException {
 
         By localizadorMiCuenta = By.xpath(" //span[@class='caret']");
         Thread.sleep(2000);
@@ -122,8 +123,6 @@ public class CasosDePrueba {
         System.out.println("Actual: "+ resultadoActual);
         Assertions.assertEquals(resultadoEsperado, resultadoActual);
 
-        //p[contains(text(),'Congratulations! Your new account has been success')]
-
 
     }
 
@@ -156,10 +155,10 @@ public class CasosDePrueba {
         Assertions.assertEquals(resultadoEsperado, resultadoActual);
 
     }
-
+    //@Disabled
     @Test
     @Order(4)
-    public void CP004_agregar_producto_carro() throws InterruptedException {
+    public void CP004_validar_mensaje_agregar_producto_carro() throws InterruptedException {
 
         By localizadorMiCuenta = By.xpath(" //span[@class='caret']");
         Thread.sleep(2000);
@@ -194,7 +193,7 @@ public class CasosDePrueba {
 
     }
 
-    @Disabled
+    //@Disabled
     @Test
     @Order(5)
     public void CP005_comprar_producto_carro() throws InterruptedException {
@@ -202,47 +201,67 @@ public class CasosDePrueba {
         By localizadorMiCuenta = By.xpath(" //span[@class='caret']");
         Thread.sleep(2000);
         WebElement btnMiCuenta = driver.findElement(localizadorMiCuenta);
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         btnMiCuenta.click();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//a[normalize-space()='Login']")).click();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys("cmoraga.ochoa@gmail.com");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("123456");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//input[@value='Login']")).click();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//i[@class='fa fa-home']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//span[@id='cart-total']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//strong[normalize-space()='Checkout']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath(" //input[@value='new']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='input-payment-firstname']")).sendKeys("Carolina");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='input-payment-lastname']")).sendKeys("Riquelme");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='input-payment-address-1']")).sendKeys("Los Alerces 123");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='input-payment-city']")).sendKeys("Temuco");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='input-payment-postcode']")).sendKeys("156789");
+        Thread.sleep(1000);
+
+        Select dropdownPais = new Select (driver.findElement(By.xpath("//select[@id='input-payment-country']")));
+        dropdownPais.selectByIndex(47);
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//div[@class='image']//img[@title='MacBook']")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//button[@id='button-cart']")).click();
+
+        Select dropdownRegion = new Select (driver.findElement(By.xpath("//select[@id='input-payment-zone']")));
+        dropdownRegion.selectByIndex(3);
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//input[@id='button-payment-address']")).click();
+        Thread.sleep(4000);
+
+        driver.findElement(By.xpath("//textarea[@name='comment']")).sendKeys("Pago mediante cuenta corriente BCI 156789");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@name='agree']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='button-payment-method']")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("//input[@id='button-confirm']")).click();
         Thread.sleep(2000);
 
 
-        String resultadoEsperado = "Success: You have added MacBook to your shopping cart!";
+        String resultadoEsperado = "Your order has been placed!";
         Thread.sleep(2000);
-        String resultadoActual = driver.findElement(By.xpath("//div[@class='alert alert-success alert-dismissible']")).getText();
+        String resultadoActual = driver.findElement(By.xpath("//h1[normalize-space()='Your order has been placed!']")).getText();
         Thread.sleep(1000);
         System.out.println("Esperado: "+ resultadoEsperado);
         System.out.println("Actual: "+ resultadoActual);
-        Assertions.assertEquals(resultadoEsperado, resultadoActual.substring(0,54));
+        Assertions.assertEquals(resultadoEsperado, resultadoActual);
 
 
     }
 }
 
 
-/*
-        driver.findElement(By.id("login-username")).sendKeys("emailTest123456789@algo.com");
-
-        Thread.sleep(1000);
-
-        driver.findElement(By.id("login-password")).sendKeys("Frre3232");
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//button[@data-testid='login-button']")).click();
-        Thread.sleep(2000);
-        String username = driver.findElement(By.xpath("//button[@data-testid='user-widget-link']")).getAttribute("aria-label");
-
-        Assertions.assertEquals("Curso Falabella Selenium",username); */
